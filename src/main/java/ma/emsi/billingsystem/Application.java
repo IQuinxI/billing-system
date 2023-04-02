4715faa600e67fdb7783fab7157568360d66bdd3
Wlahmamssi69 <wafa.lahmamssi@outlook.com>
2023-04-02 10:22:22 +0000

    add login view

--- src/main/java/ma/emsi/billingsystem/Application.java
+++ src/main/java/ma/emsi/billingsystem/Application.java
@@ -11,7 +11,7 @@ public class Application extends javafx.application.Application {
     private ImageView ImageViewBackground;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("Login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("/Fxml/Login.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Login");
